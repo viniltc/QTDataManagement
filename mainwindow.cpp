7a3694a6120ea5252b0db112d03befc8d4c4e4f7
viniltc <viniltc@gmail.com>
2019-11-07 11:21:46 +0000

    minor

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -44,7 +44,7 @@ void MainWindow::on_pushButton_clicked()
 
 void MainWindow::on_pushButton_2_clicked()
 {
-     QDomDocument document;
+     QDomDocument domDocument;
      QString  errorMsg;
      int errorLine,errorColumn;
 
@@ -68,7 +68,7 @@ void MainWindow::on_pushButton_2_clicked()
         {
             // loading
 
-            if(!document.setContent(&file,&errorMsg, &errorLine, &errorColumn))
+            if(!domDocument.setContent(&file,&errorMsg, &errorLine, &errorColumn))
             {
 
 
@@ -79,11 +79,47 @@ void MainWindow::on_pushButton_2_clicked()
             file.close();
         }
 
-        // Getting root element
-        QDomElement root = document.firstChildElement();
 
-        //retrievelements(QDomElement root, QString tag, QString att)
-        //retrievElements(root, "Dorm", "Name");
 
         qDebug() << "Reading finished";
+
+
+
+
+         domDocument.setContent(&file);
+         QDomElement topElement = domDocument.documentElement();
+         QDomNode domNode = topElement.firstChild();
+         const QString name, val;
+
+        qDebug()<<topElement.elementsByTagName("Name").at(0).firstChild().nodeValue();
+        qDebug()<<topElement.elementsByTagName("Surname").at(0).firstChild().nodeValue();
+        qDebug()<<topElement.elementsByTagName("Patient_ID").at(0).firstChild().nodeValue();
+        qDebug()<<topElement.elementsByTagName("Date").at(0).firstChild().nodeValue();
+        qDebug()<<topElement.elementsByTagName("Clinician_Note").at(0).firstChild().nodeValue();
+
+
+        while (!domNode.isNull())
+         {
+            QDomElement domElement = domNode.toElement();
+             if (!domElement.isNull())
+                {
+                 //qDebug()<<"Tag name"<<topElement.tagName();
+                // qDebug()<<domElement.tagName();
+               //  qDebug()<<domElement.toString;
+                }
+          domNode = domNode.nextSibling();
+         }
+
+
+
+
+//         <!DOCTYPE detailsentry>
+//         <subject_details>
+//          <Name>Pete</Name>
+//          <Surname>Batty</Surname>
+//          <Patient_ID>GH34TRM</Patient_ID>
+//          <Date>06/11/2019 16:00</Date>
+//          <Clinician_Note>test note to display</Clinician_Note>
+//         </subject_details>
+
 }
